271d3e1b854bfcac3a1dfca725d68716a92500fe
Tyler Jurczyk <tylerjj3@illinois.edu>
2024-03-19 23:51:46 -0500

    Initial lab6 implementation

--- lab6/lab6.cu
+++ lab6/lab6.cu
@@ -18,11 +18,59 @@
     }                                                                     \
   } while (0)
 
-__global__ void scan(float *input, float *output, int len) {
+__global__ void post_scan(float *input, float *output, float *add_amt, int len) {
+    int input_loc = blockIdx.x * blockDim.x + threadIdx.x;
+    int output_loc = input_loc;
+    if(input_loc < len)
+        output[output_loc] = input[input_loc] + add_amt;
+    if(input_loc + blockDim.x < len)
+        output[output_loc + blockDim.x] = input[input_loc + blockDim.x] + add_amt;
+}
+
+__global__ void scan(float *input, float *output, float *add_amt, int len) {
   //@@ Modify the body of this function to complete the functionality of
   //@@ the scan on the device
   //@@ You may need multiple kernel calls; write your kernels before this
   //@@ function and call them from the host
+  float T[2*BLOCK_SIZE];
+  int input_loc = blockIdx.x * blockDim.x + threadIdx.x;
+  int output_loc = input_loc;
+  int T_loc = threadIdx.x;
+
+  // Load Shared Mem
+  if(input_loc < len)
+      T[T_loc] = input[input_loc];
+  else
+      T[T_loc] = 0;
+  if(input_loc + blockDim.x < len)
+      T[T_loc + blockDim.x] = input[input_loc + blockDim.x];
+  else
+      T[T_loc + blockDim.x] = 0;
+
+  // Reduction Step
+  for(int stride = 1; stride < 2*BLOCK_SIZE; stride *= 2) {
+    __syncthreads();
+    int index = (threadIdx.x + 1) * stride * 2 - 1;
+    if(index < 2*BLOCK_SIZE && (index-stride) >= 0) {
+      T[index] += T[index-stride];
+    }
+  }
+
+  // Post Scan Step
+  for(int stride = BLOCK_SIZE/2; stride > 0; stride /= 2) {
+    __syncthreads();
+    int index = (threadIdx.x + 1) * stride * 2 - 1;
+    if(index + stride < 2*BLOCK_SIZE) {
+      T[index + stride] += T[index];
+    }
+  } 
+
+  // Writeback To Global Mem
+  if(output_loc < len)
+      output[output_loc] = T[T_loc];
+  if(output_loc + blockDim.x < len)
+      output[input_loc + blockDim.x] = T[T_loc + blockDim.x];
+  // Also need to write the add amount to the global memory
 }
 
 int main(int argc, char **argv) {
@@ -31,6 +79,7 @@ int main(int argc, char **argv) {
   float *hostOutput; // The output list
   float *deviceInput;
   float *deviceOutput;
+  float *add_amt;
   int numElements; // number of elements in the list
 
   args = wbArg_read(argc, argv);
@@ -54,10 +103,19 @@ int main(int argc, char **argv) {
                      cudaMemcpyHostToDevice));
 
   //@@ Initialize the grid and block dimensions here
+  int numBlocks = ceil(1.0f*numElements/(2*BLOCK_SIZE));
 
+  dim3 gridDim(numBlocks, 1, 1);
+  dim3 blockDim(BLOCK_SIZE, 1, 1);
+
+  cudaMalloc(&add_amt, numBlocks * sizeof(float));
+
+  scan<<<gridDim, blockDim>>>(deviceInput, deviceOutput, add_amt, numElements);
 
   //@@ Modify this to complete the functionality of the scan
   //@@ on the deivce
+
+  post_scan<<<gridDim, blockDim>>>(deviceInput, deviceOutput, add_amt, numElements);
 
   cudaDeviceSynchronize();
 
@@ -65,9 +123,10 @@ int main(int argc, char **argv) {
   wbCheck(cudaMemcpy(hostOutput, deviceOutput, numElements * sizeof(float),
                      cudaMemcpyDeviceToHost));
 
-
   //@@  Free GPU Memory
-
+  cudaFree(deviceInput);
+  cudaFree(deviceOutput);
+  cudaFree(add_amt);
 
   wbSolution(args, hostOutput, numElements);
 
